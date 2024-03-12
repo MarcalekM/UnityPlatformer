ef8845145ad3f9269d406a35b30e658306b96461
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-03-12 12:03:19 +0100

    Sounds

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,19 +6,23 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private GameObject bloodDrops;
     [SerializeField] private float maxHealth = 10f;
     [SerializeField] private Slider healthBar;
+    [SerializeField] private AudioClip playerGrunt;
     private float currentHealt;
+    private AudioSource audioSource;
 
     private void Start()
     {
         currentHealt = maxHealth;
         healthBar.maxValue = maxHealth;
         healthBar.value = maxHealth;
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void TakeDamage(float damage)
     {
         currentHealt -= damage;
         healthBar.value = currentHealt;
+        audioSource.PlayOneShot(playerGrunt);
         Instantiate(bloodDrops, transform.position, transform.rotation);
 
         if (currentHealt <= 0)
--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -5,12 +5,18 @@ public class ProjectileController : MonoBehaviour
     [SerializeField] private float projectileSpeed = 15f;
 
     private Rigidbody2D rigidbody2d;
+    private AudioSource audioSource;
 
     private void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
         rigidbody2d.AddForce(new Vector2(transform.rotation.z == 0 ? 1 : -1, 0) * projectileSpeed, ForceMode2D.Impulse);
     }
 
-    public void Stop() => rigidbody2d.velocity = new(0, 0);
+    public void Stop()
+    {
+        rigidbody2d.velocity = new(0, 0);
+        audioSource.mute = true;
+    }
 }
